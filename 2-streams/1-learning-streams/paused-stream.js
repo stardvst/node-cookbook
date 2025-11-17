f80d395b492c21235b30443e46c34f3b8619b071
stardvst <lilia.manukyan@hotmail.com>
2025-11-17 21:09:54 +0400

    interacting with paused stream

---
+++ 2-streams/1-learning-streams/paused-stream.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const rs = fs.createReadStream("./file.txt");
+
+rs.on("readable", () => {
+	let data = rs.read();
+	while (data !== null)
+	{
+		console.log("Read chunk:", data);
+		data = rs.read();
+	}
+});
+
+rs.on("end", () => { console.log("Done reading file."); });
