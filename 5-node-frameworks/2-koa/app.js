6426ac6a1b8694b429762056ffa29f32507c01af
stardvst <lilia.manukyan@hotmail.com>
2025-12-20 22:26:15 +0400

    added koa views

--- 5-node-frameworks/2-koa/app.js
+++ 5-node-frameworks/2-koa/app.js
@@ -1,11 +1,14 @@
 const Koa = require('koa');
 const path = require('path');
 const serve = require('koa-static');
+const views = require('koa-views');
 const index = require('./routes/index');
 
 const PORT = process.env.PORT || 3000;
 
 const app = new Koa();
+
+app.use(views(path.join(__dirname, 'views'), { extension: 'ejs' }));
 
 app.use(serve(path.join(__dirname, 'public')));
 
--- 5-node-frameworks/2-koa/routes/index.js
+++ 5-node-frameworks/2-koa/routes/index.js
@@ -2,17 +2,10 @@ const Router = require('koa-router');
 const router = new Router();
 
 router.get('/', async function(ctx) {
-  const title = 'Koa.js Framework';
-  ctx.body = `
-    <html>
-      <head><title>${title}</title></head>
-      <link rel="stylesheet" href="styles.css">
-      <body>
-        <h1>${title}</h1>
-        <p>Welcome to the Koa.js framework example!</p>
-      </body>
-    </html>
-  `;
+  ctx.state = {
+    title: 'Koa.js Framework'
+  };
+  await ctx.render('index');
 });
 
 module.exports = router;
